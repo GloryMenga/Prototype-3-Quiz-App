545a89299e57200f408aa5d5a93d5d4badc55853
GloryMenga <glory.menga@student.ehb.be>
2024-11-11 15:57:17 +0100

    Setup react native app done

---
+++ QuizApp/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "yellow",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hey mijn naam is glory</Text>
+    </View>
+  );
+}
